4009de13f7634362becbe0a28def6e3dfbce5e45
mat227 <heroimatheuslima2020@hotmail.com>
2021-09-13 22:32:22 -0300

    okokok

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 app.get('/produto', async (req, resp) => {
     try {
-        let produto = await db.tb_produto.findAll();
+        let produto = await db.tb_produto.findAll({order: [["id_produto","desc"]]});
         resp.send(produto);
     } catch (e) {
         resp.send({ erro: 'Ocorreu um erro!' })
@@ -17,7 +17,56 @@ app.get('/produto', async (req, resp) => {
 
 app.post('/produto', async (req, resp) => {
     try {
+        
         let prod = req.body;
+       
+        if(isNaN(prod.avaliacao) || prod.avaliacao < 0 || prod.avaliacao > 10) {
+            return resp.send({erro: 'O campo Avaliação inválido'});
+        }
+
+        if(isNaN(prod.precoDe) || prod.precoDe < 0) {
+            return resp.send({erro: 'O campo Preço DE inválido'});
+        }
+
+        if(isNaN(prod.precoPor) || prod.precoPor < 0) {
+            return resp.send({erro: 'O campo Preço POR inválido'});
+        }
+
+        if(isNaN(prod.estoque) || prod.estoque < 0) {
+            return resp.send({erro: 'O campo Estoque inválido'});
+        }
+        if(prod.nomeProduto == "") {
+            return resp.send({erro: 'O campo Nome é obrigatório'})
+        }
+        if(prod.categoria == "") {
+            return resp.send({erro: 'O campo Categoria é obrigatório'})
+        }
+        if(prod.avaliacao == "") {
+            return resp.send({erro: 'O campo Avaliação é obrigatório'})
+        }
+        if(prod.imagem == "") {
+            return resp.send({erro: 'O campo Link imagem é obrigatório'})
+        }
+        if( prod.descricao == "") {
+            return resp.send({erro: 'O campo Descrição é obrigatório'})
+        }
+        if(prod.precoDe == "") {
+            return resp.send({erro: 'O campo Preco DE é obrigatório'})
+        }
+        if( prod.precoPor == "") {
+            return resp.send({erro: 'O campo Preco POR é obrigatório'})
+        }
+        if(prod.estoque == "") {
+            return resp.send({erro: 'O campo Estoque é obrigatório'})
+        }
+
+
+
+
+        let contem = await db.tb_produto.findOne({where: {nm_produto: prod.nomeProduto}});
+        if(contem != null) {
+            return resp.send({erro: 'Produto ja existe!'});
+        }
 
         let r = await db.tb_produto.create({
             nm_produto: prod.nomeProduto,
